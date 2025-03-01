13f56f30b3e02b2c1cbf33f57d3fda92860b4234
Hudson <hudsonhendry77@gmail.com>
2025-03-01 15:58:17 +0530

    update: description

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Virtual Lab",
-  description: "Interactive learning platform for algorithms",
+  description: "Interactive learning platform for Machine algorithms",
 }
 
 export default function RootLayout({
