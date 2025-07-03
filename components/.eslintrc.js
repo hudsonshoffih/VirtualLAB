e7b2831773be3b4e4e9cf76bb3c6784a6d862669
Hudson <hudsonhendry77@gmail.com>
2025-07-03 14:44:12 +0530

    update: eslintric.js

--- components/.eslintrc.js
+++ components/.eslintrc.js
@@ -6,6 +6,7 @@ module.exports = {
       "@typescript-eslint/no-explicit-any": "off",
       "@typescript-eslint/no-unused-vars": "off",
       "@next/next/no-img-element": "off",
+      "@typescript-eslint/no-empty-object-type": "off"
     },
   }
   
